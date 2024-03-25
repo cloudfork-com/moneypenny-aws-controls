5fa6d3c790d537b80ecc8d39a57986d298c5e8b5
Ernest Micklei <ernest@ag5.com>
2024-03-25 07:21:40 +0100

    move logic to internal

--- cmd/awscontrols/main.go
+++ cmd/awscontrols/main.go
@@ -1,159 +1,35 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
 	"flag"
 	"log/slog"
 	"os"
 	"slices"
 	"time"
 
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/ecs"
-	"github.com/aws/aws-sdk-go-v2/service/ecs/types"
 	"github.com/cloudfork-com/moneypenny-aws-controls/internal/mac"
 	"github.com/lmittmann/tint"
 )
 
 var servicesInput = flag.String("services", "", "description of service schedules")
 
-var dryRun = true
-var reporting = false
-
-var serviceTagName = "moneypenny"
-
 func main() {
 	flag.Parse()
 	setupLog()
 
+	pe, err := mac.NewPlanExecutor(*servicesInput)
+	if err != nil {
+		return
+	}
 	if slices.Contains(os.Args, "apply") {
-		slog.SetDefault(slog.With("exec", "APPLY"))
-		dryRun = false
+		pe.Apply()
+		return
 	} else if slices.Contains(os.Args, "report") {
-		slog.SetDefault(slog.With("exec", "REPORT"))
-		reporting = true
+		pe.Report()
+		return
 	} else {
-		slog.SetDefault(slog.With("exec", "PLAN"))
-	}
-
-	// build weekplan
-	wp := new(mac.WeekPlan)
-	plans := []*mac.ServicePlan{}
-
-	if len(*servicesInput) == 0 {
-		slog.Info("no local service plans")
-	} else {
-		slog.Info("reading service plans", "file", *servicesInput)
-		data, err := os.ReadFile(*servicesInput)
-		if err != nil {
-			slog.Error("read fail", "err", err)
-			return
-		}
-		err = json.Unmarshal(data, &plans)
-		if err != nil {
-			slog.Error("parse fail", "err", err)
-			return
-		}
-		for _, each := range plans {
-			if err := each.Validate(); err != nil {
-				slog.Error("validate fail", "err", err)
-				return
-			}
-			slog.Info("adding service plan", "service", each.ARN, "disabled", each.Disabled)
-			wp.AddServicePlan(*each)
-		}
-	}
-
-	// Load the Shared AWS Configuration (~/.aws/config)
-	cfg, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		slog.Error("config fail", "err", err)
+		pe.Plan()
 		return
-	}
-	client := ecs.NewFromConfig(cfg)
-
-	allServices, err := mac.AllServices(client)
-	if err != nil {
-		slog.Error("AllServices fail", "err", err)
-		return
-	}
-	for _, each := range allServices {
-		input := mac.TagValue(each, serviceTagName)
-		if input == "" {
-			continue
-		}
-		sp := new(mac.ServicePlan)
-		sp.ARN = *each.ServiceArn
-		slog.Info("adding service plan", "service", *each.ServiceArn, "crons", input)
-		if input == "" {
-			slog.Warn("invalid moneypenny tag value", "value", input, "err", err)
-			continue
-		}
-		chgs, err := mac.ParseStateChanges(input)
-		if err != nil {
-			slog.Warn("invalid moneypenny tag value", "value", input, "err", err)
-			continue
-		}
-		sp.TagValue = input
-		sp.StateChanges = chgs
-		plans = append(plans, sp)
-		wp.AddServicePlan(*sp)
-	}
-	if !reporting {
-		now := time.Now()
-		for _, each := range plans {
-			if each.Disabled {
-				slog.Warn("disabled plan", "service", each.ARN)
-				continue
-			}
-			event, ok := wp.LastScheduledEventAt(each.Service, now)
-			if ok {
-				lastStatus := mac.ServiceStatus(client, each.Service)
-				if lastStatus == mac.Unknown {
-					exitsInCluster := slices.ContainsFunc(allServices, func(existing types.Service) bool {
-						return *existing.ServiceArn == each.ARN
-					})
-					if exitsInCluster {
-						slog.Warn("service has unknown last status, assume it is stopped", "name", each.Service.Name())
-						lastStatus = mac.Stopped
-					} else {
-						slog.Warn("service does not exist, update your configuration", "name", each.Service.Name())
-						continue
-					}
-				}
-				isRunning := lastStatus == mac.Running
-				if event.DesiredState != mac.Running && isRunning {
-					slog.Info("service is running but must be stopped", "name", each.Service.Name(), "state", lastStatus, "crons", each.TagValue)
-					if dryRun {
-						continue
-					}
-					if err := mac.StopService(client, each.Service); err != nil {
-						slog.Error("failed to stop service", "err", err, "name", each.Service.Name(), "state", lastStatus, "crons", each.TagValue)
-					}
-				} else if event.DesiredState == mac.Running && !isRunning {
-					slog.Info("service must be running", "name", each.Service.Name(), "state", lastStatus, "crons", each.TagValue)
-					if dryRun {
-						continue
-					}
-					if err := mac.StartService(client, each.Service); err != nil {
-						slog.Error("failed to start service", "err", err, "name", each.Service.Name(), "state", lastStatus, "crons", each.TagValue)
-					}
-				} else {
-					slog.Info("service is in expected state", "name", each.Service.Name(), "state", event.DesiredState, "crons", each.TagValue)
-				}
-			}
-		}
-	}
-	if !reporting {
-		return
-	}
-	rout, _ := os.Create("schedule.html")
-	defer rout.Close()
-	slog.Info("write schedule")
-	rep := mac.Reporter{}
-	if err := rep.WriteOn(wp, rout); err != nil {
-		slog.Error("schedule report failed", "err", err)
 	}
 }
 
---
+++ internal/mac/execution.go
@@ -0,0 +1,176 @@
+package mac
+
+import (
+	"context"
+	"encoding/json"
+	"log/slog"
+	"os"
+	"slices"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ecs"
+	"github.com/aws/aws-sdk-go-v2/service/ecs/types"
+)
+
+var serviceTagName = "moneypenny"
+
+type PlanExecutor struct {
+	configFile string
+	dryRun     bool
+	reporting  bool
+	wp         *WeekPlan
+	plans      []*ServicePlan
+	client     *ecs.Client
+}
+
+func NewPlanExecutor(localPlanFilename string) (*PlanExecutor, error) {
+	p := &PlanExecutor{configFile: localPlanFilename, wp: new(WeekPlan), dryRun: true, reporting: false}
+	p.loadServicePlans()
+	p.createClient()
+	return p, nil
+}
+
+func (p *PlanExecutor) Plan() error {
+	slog.SetDefault(slog.With("exec", "PLAN"))
+	p.dryRun = true
+	return p.exec()
+}
+func (p *PlanExecutor) Apply() error {
+	slog.SetDefault(slog.With("exec", "APPLY"))
+	p.dryRun = false
+	return p.exec()
+}
+func (p *PlanExecutor) Report() error {
+	slog.SetDefault(slog.With("exec", "REPORT"))
+
+	rout, _ := os.Create("schedule.html")
+	defer rout.Close()
+	slog.Info("write schedule")
+	rep := Reporter{}
+	if err := rep.WriteOn(p.wp, rout); err != nil {
+		slog.Error("schedule report failed", "err", err)
+		return err
+	}
+	return nil
+}
+
+func (p *PlanExecutor) loadServicePlans() error {
+	if len(p.configFile) == 0 {
+		slog.Info("no local service plans")
+	} else {
+		slog.Info("reading service plans", "file", p.configFile)
+		data, err := os.ReadFile(p.configFile)
+		if err != nil {
+			slog.Error("read fail", "err", err)
+			return err
+		}
+		err = json.Unmarshal(data, &p.plans)
+		if err != nil {
+			slog.Error("parse fail", "err", err)
+			return err
+		}
+		for _, each := range p.plans {
+			if err := each.Validate(); err != nil {
+				slog.Error("validate fail", "err", err)
+				return err
+			}
+			slog.Info("adding service plan", "service", each.ARN, "disabled", each.Disabled)
+			p.wp.AddServicePlan(*each)
+		}
+	}
+	return nil
+}
+
+func (p *PlanExecutor) createClient() error {
+	// Load the Shared AWS Configuration (~/.aws/config)
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		slog.Error("config fail", "err", err)
+		return err
+	}
+	p.client = ecs.NewFromConfig(cfg)
+	return nil
+}
+
+func (p *PlanExecutor) fetchAllServices() ([]types.Service, error) {
+	allServices, err := AllServices(p.client)
+	if err != nil {
+		slog.Error("AllServices fail", "err", err)
+		return nil, err
+	}
+	for _, each := range allServices {
+		input := TagValue(each, serviceTagName)
+		if input == "" {
+			continue
+		}
+		sp := new(ServicePlan)
+		sp.ARN = *each.ServiceArn
+		slog.Info("adding service plan", "service", *each.ServiceArn, "crons", input)
+		if input == "" {
+			slog.Warn("invalid moneypenny tag value", "value", input, "err", err)
+			continue
+		}
+		chgs, err := ParseStateChanges(input)
+		if err != nil {
+			slog.Warn("invalid moneypenny tag value", "value", input, "err", err)
+			continue
+		}
+		sp.TagValue = input
+		sp.StateChanges = chgs
+		p.plans = append(p.plans, sp)
+		p.wp.AddServicePlan(*sp)
+	}
+	return allServices, nil
+}
+
+func (p *PlanExecutor) exec() error {
+	allServices, err := p.fetchAllServices()
+	if err != nil {
+		return err
+	}
+	now := time.Now()
+	for _, each := range p.plans {
+		if each.Disabled {
+			slog.Warn("disabled plan", "service", each.ARN)
+			continue
+		}
+		event, ok := p.wp.LastScheduledEventAt(each.Service, now)
+		if ok {
+			lastStatus := ServiceStatus(p.client, each.Service)
+			if lastStatus == Unknown {
+				exitsInCluster := slices.ContainsFunc(allServices, func(existing types.Service) bool {
+					return *existing.ServiceArn == each.ARN
+				})
+				if exitsInCluster {
+					slog.Info("service has unknown last status, assume it is stopped", "name", each.Service.Name())
+					lastStatus = Stopped
+				} else {
+					slog.Warn("service does not exist, update your configuration", "name", each.Service.Name())
+					continue
+				}
+			}
+			isRunning := lastStatus == Running
+			if event.DesiredState != Running && isRunning {
+				slog.Info("service is running but must be stopped", "name", each.Service.Name(), "state", lastStatus, "crons", each.TagValue)
+				if p.dryRun {
+					continue
+				}
+				if err := StopService(p.client, each.Service); err != nil {
+					slog.Error("failed to stop service", "err", err, "name", each.Service.Name(), "state", lastStatus, "crons", each.TagValue)
+				}
+			} else if event.DesiredState == Running && !isRunning {
+				slog.Info("service must be running", "name", each.Service.Name(), "state", lastStatus, "crons", each.TagValue)
+				if p.dryRun {
+					continue
+				}
+				if err := StartService(p.client, each.Service); err != nil {
+					slog.Error("failed to start service", "err", err, "name", each.Service.Name(), "state", lastStatus, "crons", each.TagValue)
+				}
+			} else {
+				slog.Info("service is in expected state", "name", each.Service.Name(), "state", event.DesiredState, "crons", each.TagValue)
+			}
+		}
+	}
+	return nil
+}
