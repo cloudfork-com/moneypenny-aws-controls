fe3ede61e8834545769207689f815778527f593c
Ernest Micklei <ernest@ag5.com>
2024-03-30 08:16:19 +0100

    no time and level in passthrough log

--- lambda/main.go
+++ lambda/main.go
@@ -13,6 +13,8 @@ import (
 	"github.com/cloudfork-com/moneypenny-aws-controls/internal/mac"
 )
 
+var Version string = "dev"
+
 func HandleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	resp := events.APIGatewayProxyResponse{
 		Headers:    map[string]string{"Content-Type": "text/html; charset=UTF-8"},
@@ -20,7 +22,7 @@ func HandleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (even
 
 	// setup logging
 	logBuffer := new(bytes.Buffer)
-	stdoutHandler := slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelDebug})
+	stdoutHandler := slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelDebug, ReplaceAttr: removeTimeAndLevel})
 	logHandler := htmlslog.New(logBuffer, htmlslog.Options{Level: slog.LevelDebug, Title: "moneypenny-aws-controls", PassthroughHandler: stdoutHandler})
 	slog.SetDefault(slog.New(logHandler))
 
@@ -55,4 +57,11 @@ func HandleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (even
 
 func main() {
 	lambda.Start(HandleRequest)
+}
+
+func removeTimeAndLevel(groups []string, a slog.Attr) slog.Attr {
+	if a.Key == "time" || a.Key == "level" {
+		return slog.Attr{}
+	}
+	return a
 }
