28b8cc8fa29e4ac52902ab3fa0c21d5072e99909
Ernest Micklei <ernest@ag5.com>
2024-04-18 10:46:30 +0200

    initial cdk setup

--- infra/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/cloudfork-com/moneypenny-aws-controls/infra
-
-go 1.22
--- infra/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import awscdk "github.com/aws/aws-cdk-go/awscdk/v2"
-
-// https://pkg.go.dev/github.com/aws/aws-cdk-go/awscdk/v2
-func main() {
-	app := awscdk.NewApp()
-	stack := awscdk.NewStack(app, jsii.String("TestStack"))
-}
