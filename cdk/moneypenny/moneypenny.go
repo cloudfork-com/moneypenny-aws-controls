d76e5c3eeed878ef8803b8d3a043414a71728849
ernest micklei <ernest.micklei@gmail.com>
2025-03-18 17:07:20 +0100

    add lambda integration

--- cdk/moneypenny/moneypenny.go
+++ cdk/moneypenny/moneypenny.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigatewayv2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigatewayv2integrations"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsiam"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 )
@@ -60,7 +63,7 @@ func NewMoneypennyStack(scope constructs.Construct, id string, props *Moneypenny
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
 	role := awsiam.NewRole(stack, jsii.String("moneypenny-aws-controls-role"), &awsiam.RoleProps{
-		AssumedBy: awsiam.NewServicePrincipal(jsii.String("ecs.amazonaws.com"), nil),
+		AssumedBy: awsiam.NewServicePrincipal(jsii.String("lambda.amazonaws.com"), nil),
 	})
 	role.AddToPolicy(awsiam.NewPolicyStatement(&awsiam.PolicyStatementProps{
 		Effect: awsiam.Effect_ALLOW,
@@ -80,19 +83,37 @@ func NewMoneypennyStack(scope constructs.Construct, id string, props *Moneypenny
 	}))
 
 	// Add a managed policy to a role you can use
-	role.AddManagedPolicy(
-		// which to use?
-		awsiam.ManagedPolicy_FromAwsManagedPolicyName(jsii.String("service-role/AWSLambdaBasicExecutionRole")),
-	)
+	// role.AddManagedPolicy(awsiam.ManagedPolicy_FromAwsManagedPolicyName(jsii.String("AmazonECS_FullAccess")))
+	// role.AddManagedPolicy(awsiam.ManagedPolicy_FromAwsManagedPolicyName(jsii.String("service-role/AWSLambdaBasicExecutionRole")))
 
-	/**
-		// https://aws.amazon.com/blogs/compute/migrating-aws-lambda-functions-from-the-go1-x-runtime-to-the-custom-runtime-on-amazon-linux-2/
-		awslambda.NewFunction(stack, jsii.String("moneypenny-aws-controls"), &awslambda.FunctionProps{
-			Code:         awslambda.Code_FromAsset(jsii.String("../../lambda"), nil), //folder where bootstrap executable is located
-			Runtime:      awslambda.Runtime_PROVIDED_AL2023(),
-			Handler:      jsii.String("bootstrap"),
-			Architecture: awslambda.Architecture_ARM_64(),
-		})
-	**/
+	// https://aws.amazon.com/blogs/compute/migrating-aws-lambda-functions-from-the-go1-x-runtime-to-the-custom-runtime-on-amazon-linux-2/
+	lambda := awslambda.NewFunction(stack, jsii.String("moneypenny-aws-controls"), &awslambda.FunctionProps{
+		Code:         awslambda.Code_FromAsset(jsii.String("../../lambda"), nil), //folder where bootstrap executable is located
+		Runtime:      awslambda.Runtime_PROVIDED_AL2023(),
+		Handler:      jsii.String("bootstrap"),
+		Architecture: awslambda.Architecture_ARM_64(),
+		Role:         role,
+		Description:  jsii.String("Moneypenny AWS Controls - Lambda function to control the desired count of ECS services"),
+		Environment: &map[string]*string{
+			"Variables": jsii.String("{TIME_ZONE=Europe/Amsterdam,BASIC_USER=nil,BASIC_PASSWORD=zork}"),
+		},
+	})
+
+	// Create an API Gateway
+	awsapigatewayv2.NewHttpApi(stack, jsii.String("moneypenny-aws-controls-api"), &awsapigatewayv2.HttpApiProps{
+		ApiName: jsii.String("Moneypenny AWS Controls API"),
+		CorsPreflight: &awsapigatewayv2.CorsPreflightOptions{
+			AllowHeaders: jsii.Strings("*"),
+			AllowMethods: &[]awsapigatewayv2.CorsHttpMethod{"GET", "POST", "PUT", "DELETE"},
+			AllowOrigins: jsii.Strings("*"),
+		},
+	})
+
+	// Create a Lambda integration
+	awsapigatewayv2integrations.NewHttpLambdaIntegration(
+		jsii.String("moneypenny-aws-controls-integration"),
+		lambda,
+		&awsapigatewayv2integrations.HttpLambdaIntegrationProps{})
+
 	return stack
 }
